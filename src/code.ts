488f9e614f440af68adeafdba23a1bc8527dd923
Albert <bertismrpdl@gmail.com>
2019-10-03 19:31:04 -0400

    fix git ignore

---
+++ src/code.ts
@@ -0,0 +1,35 @@
+import { Observable } from "rxjs/Observable"
+import 'rxjs/add/operator/share';
+
+var observable = Observable.create(function subscribe(observer:any) {
+    try {
+        observer.next('Hey guys!');
+        observer.next('How are you?');
+        setInterval(() => {
+            observer.next('I am good')
+        }, 2000)
+
+    } catch (err){
+        observer.error(err);
+    }
+}).share();
+
+var observer = observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem('Completed')
+);
+
+
+setTimeout(() => {
+    var observer2 = observable.subscribe(
+        (x:any) => addItem('Subscriber 2: ' + x)
+    )
+}, 1000);
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
